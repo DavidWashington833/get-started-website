2c290dd02d180efb0048f4800492c26e7c923b98
David Washington <davidwashington833@gmail.com>
2021-06-19 20:53:38 -0300

    Remove not use pkgs, update versions and refactoring webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,36 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglify-js-plugin');
 
-module.exports = () => {
-  return {
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin(),
-        new OptimizeCSSAssetsPlugin()
-      ]
-    },
-    entry: [
-      './assets/js/main.js',
-      './assets/scss/main.scss'
+module.exports = {
+  entry: [
+    "./index.html",
+    "./assets/js/main.js",
+    "./assets/scss/main.scss",
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "html-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      }
     ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        },
-        {
-          test: /\.scss$/,
-          exclude: /node_modules/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader'
-          ]
-        },
-        {
-          test: /\.(svg|png|jpg|gif)$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8192
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin(),
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        filename: './index.html'
-      })
-    ]
-  }
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "./index.html",
+    }),
+  ]
+};
